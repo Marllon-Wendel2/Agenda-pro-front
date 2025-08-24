e71c0984f9dc3adb41673009693cce66ed07c1cf
Marllon Wendel <m.w.p.ferreira@outlook.com>
2025-08-24 15:29:07 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,90 @@
-import Image from "next/image";
+import { Breadcrumb, Layout, Menu, MenuProps } from "antd";
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons'
+import { Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+import Image from 'next/image';
+import React from "react";
+import Appointments from "./components/Agendamentos/Appointments";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const items2: MenuProps['items'] = [UserOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `Serviços`,
+      children: [
+        {
+          key:`Agendamentos`,
+          label: 'Agendamentos',
+        },
+        {
+          key:`Clientes`,
+          label: 'Clientes',
+        },
+        {
+          key:`Serviços`,
+          label: 'Serviços',
+        },
+      ]
+    };
+  },
+);
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+    <Header
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
+        boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+        padding: '0 24px',
+      }}
+    >
+      <Image
+        src="/AgendaPro.png"
+        alt="Agenda Pro Logo"
+        width={150}
+        height={100}
+      />
+    </Header>
+     <Layout>
+      {/* MENU LATERAL */}
+      <Sider
+        style={{
+          background: '#A7C7E7',
+          minHeight: 'calc(100vh - 64px)',
+          boxShadow: '2px 0 5px rgba(0,0,0,0.05)',
+        }}
+        width={200}
+      >
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ height: '100%', background: '#A7C7E7', borderRight: 0 }}
+          items={items2}
+        />
+      </Sider>
+
+      {/* ÁREA PRINCIPAL */}
+      <Layout style={{ padding: '10px 24px' }}>
+        <div
+          style={{
+            padding: 24,
+            minHeight: 360,
+            background: '#FFFFFF',
+            borderRadius: '12px',
+            boxShadow: '0 2px 6px rgba(0,0,0,0.05)',
+          }}
+        >
+          <Appointments />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </Layout>
+    </Layout>
+    </Layout>
   );
 }
